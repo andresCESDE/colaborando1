dc2f621d3a96a150c5391d4f5195cdb42b1f27b3
saldaandres <saldaandres@hotmail.es>
2023-04-20 13:55:41 -0500

    Crear la funcion division

--- src/aritmetica.js
+++ src/aritmetica.js
@@ -5,3 +5,13 @@ const sumar = (numero1, numero2) => numero1 + numero2;
 // se crea la funcion resta desde Codespaces
 
 const restar = (numero1, numero2) => numero1 - numero2;
+
+// se crea la funcion dividir desde el equipo local
+
+const dividir = (numero1, numero2) => {
+    if (numero2 == 0) {
+        console.log("Division por CERO no definida")
+        return
+    }
+    return numero1 / numero2;
+}
