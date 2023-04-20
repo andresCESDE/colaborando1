b96a1bc81f5f890857b6a1c0b4b869415808c63b
andresCESDE <131395974+andresCESDE@users.noreply.github.com>
2023-04-20 18:45:21 +0000

    Crear funcion multiplicacion desde Codespaces
    La idea con esta funcion es crear un conflicto
    con el archivo modificado en el equipo local

--- src/aritmetica.js
+++ src/aritmetica.js
@@ -5,3 +5,7 @@ const sumar = (numero1, numero2) => numero1 + numero2;
 // se crea la funcion resta desde Codespaces
 
 const restar = (numero1, numero2) => numero1 - numero2;
+
+// se crea la funcion multiplicacion desde Codespaces
+
+const multiplicar = (numero1, numero2) => numero1 * numero2;
