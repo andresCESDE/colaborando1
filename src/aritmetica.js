7d0d908fad056307455a2631d0939750c8d2b222
andresCESDE <131395974+andresCESDE@users.noreply.github.com>
2023-05-16 20:21:21 +0000

    Cambiar la funcion flecha por una declarada

--- src/aritmetica.js
+++ src/aritmetica.js
@@ -1,23 +1,6 @@
-// se crean la funcion suma
-
-const sumar = (numero1, numero2) => numero1 + numero2;
-
-// se crea la funcion resta desde Codespaces
-
-const restar = (numero1, numero2) => numero1 - numero2;
-
-
-// se crea la funcion dividir desde el equipo local
-
-const dividir = (numero1, numero2) => {
-    if (numero2 == 0) {
-        console.log("Division por CERO no definida")
-        return
-    }
-    return numero1 / numero2;
+function suma(numero1, numero2) {
+    return numero1 + numero2
 }
-
-// se crea la funcion multiplicacion desde Codespaces
 
 const multiplicar = (numero1, numero2) => numero1 * numero2;
 
@@ -35,5 +18,4 @@ const fibonacci = (posicion) => {
   return fibonacci(posicion -1) + fibonacci(posicion -2);
 }
 
-quo
 
