d5d7872bad9f23a44c44d5e25210c7fd679073bc
andresCESDE <131395974+andresCESDE@users.noreply.github.com>
2023-05-16 20:05:40 +0000

    Funcion factorial

--- src/aritmetica.js
+++ src/aritmetica.js
@@ -2,10 +2,16 @@
 
 const sumar = (numero1, numero2) => numero1 + numero2;
 
-// se crea la funcion resta desde Codespaces
-
-const restar = (numero1, numero2) => numero1 - numero2;
+// se eliminó la funcion de resta
 
 // se crea la funcion multiplicacion desde Codespaces
 
 const multiplicar = (numero1, numero2) => numero1 * numero2;
+
+// se crea la funcion de factorial
+function factorial(position) {
+    if (position == 0) {
+        return 1   
+    }
+    return position*factorial(position -1)
+}
